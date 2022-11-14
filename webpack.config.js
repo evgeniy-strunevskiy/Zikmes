0053f1527264b142ee01bcb93df7e32f4b3febba
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2022-11-14 12:14:26 +0300

    basic config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js'
+  }
+}
