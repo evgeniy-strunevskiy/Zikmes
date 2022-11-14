a788e24464a4f230a09b1daf9cdfa25e94e76ecd
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2022-11-14 14:12:17 +0300

    styles webapck.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -7,6 +8,9 @@ const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
+  mode,
+  target,
+  devtool,
   entry: {
     main: path.resolve(__dirname, 'src', 'index.js'),
   },
@@ -18,6 +22,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].scss'
     })
   ],
   module: {
@@ -26,6 +33,23 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')]
+              }
+            }
+          },
+          "sass-loader",
+        ],
+      },
+      
     ],
   },
 }
